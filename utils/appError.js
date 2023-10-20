36f08687e864bf889409b9d47124488c8d3bde46
Brayan Zavala <zavala_243@hotmail.com>
2023-10-20 16:25:34 -0700

    Los logs ya se registran en error.log

--- utils/appError.js
+++ utils/appError.js
@@ -1,33 +1,37 @@
-const winston = require ('winston');
+const winston = require('winston');
 
 const logger = winston.createLogger({
     level: 'error',
     format: winston.format.json(),
     transports: [
-        new  winston.transports.File({filename: 'error.log'})
+        new winston.transports.File({ filename: 'error.log' })
     ]
 });
 
-class AppError extends Error{
-    constructor(mesagge, statusCode) {
-        super(mesagge);
+class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message);
         this.statusCode = statusCode;
         this.status = 'fail';
-        this.isOptional = true;
+        this.isOperational = true;
 
         Error.captureStackTrace(this, this.constructor);
     }
-
 }
 
 const globalHandlerError = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    logger.error()
-}
+    logger.error(err); // Pasar el error al logger
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+};
 
 module.exports = {
     AppError,
     globalHandlerError
-}
+};
